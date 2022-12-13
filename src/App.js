68670f4672b9cb6a78539b4e0af010eafaa71786
Sakha31 <sakhaprabaswara31@gmail.com>
2022-12-13 23:37:42 +0700

    tugasreact

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://img.lovepik.com/original_origin_pic/18/08/29/647d6dbcc5c8ec3ea2d0076d7a429e00.png_wh300.png" className="App-logo" alt="logo" />
+        <p>
+          Muhammad<code>Sakha</code> Prabaswara
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
